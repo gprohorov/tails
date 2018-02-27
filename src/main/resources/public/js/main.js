481f257445614238a74726dc2726694e8a32de0c
george2 <yuramail222@gmail.com>
2018-02-27 17:36:37 +0200

    Workable with postconstract

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -20,7 +20,14 @@ app.controller("AppCtrl", function($scope, $http){
         console.log(response);
     });
 
-
+    this.del = function del(id) {
+        $http.get('/api/student/del?id='+id).then(function (response){
+            //  $http.get('http://localhost:8080/api/students').then(function (response){
+            // $scope.students=response.data;
+            window.alert('Student ' + response.data.lastName + ' ' + response.data.firstName + ' has been succesfully deleted!');
+            window.location.reload();
+        });
+    }
 
 
 
